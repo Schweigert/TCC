163e661eac1e69fc1e26e7d0ceb4e853f3ee253c
schweigert <marlon.henry@magrathealabs.com>
2019-03-17 17:17:35 -0300

    Plot with gnuplot

--- metricas/classify.rb
+++ metricas/classify.rb
@@ -2,18 +2,29 @@ require 'csv'
 
 klasses = {}
 
-file_name = gets.chomp
+file_name = ARGV[0].chomp
 
 CSV.foreach(file_name) do |row|
     klasses[row[0]] ||= {}
     klasses[row[0]][row[1]] = row[2] unless row[2].nil?
 end
 
+peer_time = {}
+types = []
 klasses.each do |klass, data|
-    CSV.open("#{klass}.#{file_name}", 'wb') do |csv|
-        csv << ['time', 'value']
-        data.each do |time, value|
-            csv << [time, value]
-        end
+    data.each do |time, value|
+        peer_time[time] ||= {}
+        peer_time[time][klass] = value.to_f
+        types << klass
+    end
+end
+
+types.uniq!
+
+File.open("#{file_name}.data", 'w') do |file|
+    file.puts types.join(' ')
+    peer_time.keys.sort.each do |time|
+        data = peer_time[time]
+        file.puts(([time.to_s.gsub('2019', '19').to_s.gsub('-', '/')] + types.map { |type| data[type] || -100 }).join(' '))
     end
 end
